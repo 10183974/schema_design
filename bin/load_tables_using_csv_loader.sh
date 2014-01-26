e357dca4bcc5ac6247d8cd23b11e85e6ba8236d8
Abhishek Dubey <abhishek1015@gmail.com>
2014-01-26 14:50:07 -0500

    Update load_tables_using_csv_loader.sh

--- bin/load_tables_using_csv_loader.sh
+++ bin/load_tables_using_csv_loader.sh
@@ -6,7 +6,7 @@ export raw_data_dir=/raw_data/
 export project_home=~/git/schema_design/
 
 # commands for converting raw_data to csv format
-$HADOOP_HOME/bin/hadoop  jar /home/hadoop/hadoop/contrib/streaming/hadoop-streaming-1.2.1.jar -input $raw_data_dir -output $csv_data_dir -mapper $project_home -reducer org.apache.hadoop.mapred.lib.IdentityMapper
+$HADOOP_HOME/bin/hadoop  jar /home/hadoop/hadoop/contrib/streaming/hadoop-streaming-1.2.1.jar -input $raw_data_dir -output $csv_data_dir -mapper $project_home/bin/rawdatatocsv.sh -reducer org.apache.hadoop.mapred.lib.IdentityMapper
 
 
 # commands for loading data
