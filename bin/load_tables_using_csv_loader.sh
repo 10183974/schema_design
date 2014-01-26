8ed416527d23518b9066378be6df047c1c5e6893
Abhishek Dubey <abhishek1015@gmail.com>
2014-01-26 14:45:42 -0500

    Update load_tables_using_csv_loader.sh

--- bin/load_tables_using_csv_loader.sh
+++ bin/load_tables_using_csv_loader.sh
@@ -1,7 +1,15 @@
 #! /bin/bash
 
 export phoenix_home=/home/hadoop/git/phoenix
-export csv_dir=/csv_files
+export csv_data_dir=/csv_files/
+export raw_data_dir=/raw_data/ 
+export project_home=~/git/schema_design/
+
+# commands for converting raw_data to csv format
+$HADOOP_HOME/bin/hadoop  jar /home/hadoop/hadoop/contrib/streaming/hadoop-streaming-1.2.1.jar -input $raw_data_dir -output $csv_data_dir -mapper $project_home -reducer org.apache.hadoop.mapred.lib.IdentityMapper
+
+
+# commands for loading data
 
 $phoenix_home/bin/csv-bulk-loader.sh -i $csv_dir/customer.csv -t customer -zk master:2181 -hd hdfs://master:54310 -mr master:54311
 
