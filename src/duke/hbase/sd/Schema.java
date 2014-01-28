a665ff948f1261cb69b1d915d71aaf6da3390eb6
abhisdub <abhisdub@cs.duke.edu>
2014-01-28 18:32:33 -0500

    update

--- src/duke/hbase/sd/Schema.java
+++ src/duke/hbase/sd/Schema.java
@@ -1,12 +1,45 @@
 package duke.hbase.sd;
 
+import java.util.ArrayList;
+
 public class Schema {
 
-  /**
-   * @param args
-   */
-  public static void main(String[] args) {
-    // TODO Auto-generated method stub
+  static int SCHEMA_COUNT = 0;
+
+  int id = 0;
+  ArrayList<Table> tables = new ArrayList<Table>();
+
+  public Schema() {
+    // TODO Auto-generated constructor stub
+  }
+
+  public Schema(ArrayList<Table> tables) {
+    super();
+    this.id = SCHEMA_COUNT++;
+    this.tables = tables;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public ArrayList<Table> getTables() {
+    return tables;
+  }
+
+  public void setTables(ArrayList<Table> tables) {
+    this.tables = tables;
+  }
+
+  // initialize from the hbase connection
+  public Schema init(ArrayList<String> table_list) {
+    Schema schema = new Schema();
+    this.id = SCHEMA_COUNT++;
+    return schema;
 
   }
 
--- src/duke/hbase/sd/Table.java
+++ src/duke/hbase/sd/Table.java
@@ -1,15 +1,44 @@
 package duke.hbase.sd;
 
+import java.util.ArrayList;
+
 public class Table {
 
   String name;
+  ArrayList<Column> columns = new ArrayList<Column>();
+  int rowcount = 0;
 
-  /**
-   * @param args
-   */
-  public static void main(String[] args) {
-    // TODO Auto-generated method stub
+  public Table() {
+    // TODO Auto-generated constructor stub
+  }
 
+  public Table(String name, ArrayList<Column> columns, int rowcount) {
+    super();
+    this.name = name;
+    this.columns = columns;
+    this.rowcount = rowcount;
+  }
+
+  // initialize table object from hbase table
+  public Table init(String tablename) {
+    Table table = new Table();
+    return table;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public ArrayList<Column> getColumns() {
+    return columns;
+  }
+
+  public void setColumns(ArrayList<Column> columns) {
+    this.columns = columns;
   }
 
 }
