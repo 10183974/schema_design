d7bd161fc50e143f9280d183032d9699b56b686d
abhisdub <abhisdub@cs.duke.edu>
2014-01-28 00:28:54 -0500

    adding java files

---
+++ src/duke/hbase/sd/Schema.java
@@ -0,0 +1,13 @@
+package duke.hbase.sd;
+
+public class Schema {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
---
+++ src/duke/hbase/sd/Table.java
@@ -0,0 +1,13 @@
+package duke.hbase.sd;
+
+public class Table {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
