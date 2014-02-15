9f6bd9f9a6daa16b731372e15be8d94a61b1d976
Weizheng Zhou <wz33@duke.edu>
2014-02-15 00:11:09 -0500

    upload MODIFIED pdgf package, plus DataGenerator

---
+++ src/duke/hbase/cm/tdg/pdgf/DataGenerator.java
@@ -0,0 +1,54 @@
+import pdgf.Controller;
+
+import java.lang.ProcessBuilder; 
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
+import pdgf.core.exceptions.ConfigurationException;
+import pdgf.core.exceptions.InvalidArgumentException;
+import pdgf.core.exceptions.InvalidElementException;
+import pdgf.core.exceptions.InvalidStateException;
+import pdgf.core.exceptions.XmlException;
+
+public class DataGenerator{
+  public static void main(String[] agrs){
+  Controller controller = null; 
+  try {
+       controller = new Controller();
+       controller.executeCommand(new String[] {"load","config/tpchSchema.xml"});
+       controller.executeCommand(new String[] {"start"});
+      // controller.executeCommand(new String[] {"exit"} );
+   } catch (IOException e) {
+       e.printStackTrace();
+   } catch (InvalidArgumentException e) {
+       e.printStackTrace();
+   } catch (InstantiationException e) {
+       e.printStackTrace();
+   } catch (IllegalAccessException e) {
+       e.printStackTrace();
+   } catch (XmlException e) {
+       e.printStackTrace(); 
+   } catch (ConfigurationException e) {
+       e.printStackTrace();
+   } catch (ParserConfigurationException e) {
+       e.printStackTrace();       
+   } catch (SAXException e) {
+      e.printStackTrace();
+   } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+   } catch (InvalidElementException e) {
+      e.printStackTrace();
+   } catch (InvalidStateException e) {
+      e.printStackTrace();
+   }
+
+  System.out.println("Hello World!");
+
+   
+  }
+
+
+} 
+
